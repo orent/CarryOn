45235aafbcd2f9c684e20ad65af91353f6cc374f
Oren Tirosh <orent@hishome.net>
2024-11-05 22:13:37 +0200

    python 3.13

--- carryon.py
+++ carryon.py
@@ -62,7 +62,13 @@ BOOTSTRAP_CODE = b"""exec(compile(
     # The zip from which __main__ was loaded:
     open(__loader__.archive, 'rb').read(
         # minimum offset = size of file before zip start:
-        min(f[4] for f in __loader__._files.values())
+        min(f[4] for f in
+            (
+                __loader__._files
+                if hasattr(__loader__, '_files') else
+                __loader._get_files()
+            ).values()
+        )
     ).decode('utf8', 'surrogateescape'),
     __loader__.archive,     # set filename in code object
     'exec'                  # compile in 'exec' mode
